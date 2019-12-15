586e6d16071b6ea14f1a54e3cc5147fc54e924c8
Ruslan Manaev <manavrion@gmail.com>
2019-12-15 19:33:38 +0300

    Add packaging

--- lock3/build.sh
+++ lock3/build.sh
@@ -5,3 +5,5 @@ cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=../../toolchain.cmake -DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi" ../clang/llvm
 cmake --build . -j 64
 cmake --build .
+cmake -DCMAKE_INSTALL_PREFIX=install -P cmake_install.cmake
+tar -czvf clang_of_lock3.tar.gz install/
