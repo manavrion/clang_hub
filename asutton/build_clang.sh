4901e4d079644f1dcec2e57ecacc4a37ca10b5b8
Ruslan Manaev <manavrion@gmail.com>
2020-02-19 21:01:50 +0300

    make package of clang of asutton

--- asutton/build_clang.sh
+++ asutton/build_clang.sh
@@ -3,5 +3,7 @@ rm -rf clang_build
 mkdir clang_build
 cd clang_build
 cmake -DCMAKE_TOOLCHAIN_FILE=../../toolchain.cmake -DLLVM_CONFIG=../llvm_build/install/bin/llvm-config ../clang
-cmake --build . --target clang -j 64
-cmake --build . --target clang
+cmake --build . -j 64
+cmake --build .
+cmake -DCMAKE_INSTALL_PREFIX=install -P cmake_install.cmake
+tar -czvf clang_of_asutton.tar.gz install/
